205fb14a0d105480d15ef371fb1eb734c681316c
David Rettie <drettie@qmetric.co.uk>
2022-05-15 23:19:02 +0100

    add some bad code for sonar to check

---
+++ src/main/java/com/rettie/rettie/api/PingController.java
@@ -0,0 +1,19 @@
+package com.rettie.rettie.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+    @GetMapping("/ping")
+	public String ping() {
+        if (1+1==2) {
+            System.out.println("asdasdasdasd");
+          } if (1+1==2) {
+            System.out.println("asdasdsasd");
+          }
+		return "test";         // asdasdoasioasidjasodij
+	}
+
+}
