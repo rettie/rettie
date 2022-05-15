9fd9491a4e1c1e06788ee86fbaee8b78eb247eca
David Rettie <drettie@qmetric.co.uk>
2022-05-15 22:28:27 +0100

    init project

---
+++ src/main/java/com/rettie/rettie/RettieApplication.java
@@ -0,0 +1,13 @@
+package com.rettie.rettie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RettieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RettieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rettie/rettie/RettieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rettie.rettie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RettieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
